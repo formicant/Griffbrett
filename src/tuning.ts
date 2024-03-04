6d50f8967263d668567c501a7726da5b8bc13577
formicant <formicant@gmail.com>
2024-03-04 14:20:10 +0700

    Tuning class and tests

---
+++ src/tuning.ts
@@ -0,0 +1,49 @@
+import { Note, noteNamePattern } from './note';
+import { Chord } from './chord';
+
+const splitRegex = /\s+|(?=[A-Z])/; // split by whitespace or before uppercase letters
+
+
+function getFrets(openString: Note, allowedPitches: Set<number>, fretCount: number): Array<Note | null> {
+  const frets = [];
+  for (let fretIndex = 0; fretIndex <= fretCount; fretIndex++) {
+    const note = openString.addInterval(fretIndex);
+    frets.push(allowedPitches.has(note.pitchClass) ? note : null);
+  }
+  return frets;
+}
+
+
+export function getGroup(rootNote: Note, note: Note): number | undefined {
+  if (note.octave !== undefined) {
+    return Math.floor((note.octave * 12 + note.pitchClass - rootNote.pitchClass) / 12);
+  } else {
+    return undefined;
+  }
+}
+
+
+export class Tuning {
+  readonly description: string;
+  readonly openStrings: Note[];
+  
+  constructor(description: string) {
+    this.openStrings = description
+      .split(splitRegex)
+      .filter(n => n !== '')
+      .map(n => new Note(n));
+    if (this.openStrings.length === 0) {
+      throw new Error('At least one open string required');
+    }
+    this.description = this.openStrings.join(' ');
+  }
+  
+  toString(): string {
+    return `Tuning("${this.description}")`;
+  }
+  
+  getFretboard(chord: Chord, fretCount: number = 12): Array<Note | null>[] {
+    const allowedPitches = new Set(chord.notes.map(n => n.pitchClass));
+    return this.openStrings.map(s => getFrets(s, allowedPitches, fretCount));
+  }
+}
---
+++ tests/tuning.test.ts
@@ -0,0 +1,124 @@
+import { Note } from '../src/note';
+import { Chord } from '../src/chord';
+import { Tuning, getGroup } from '../src/tuning';
+
+describe('Tuning class', () => {
+  
+  test.each([
+    '', '\n \t'
+  ])('empty', description => {
+    const expectedError = Error('At least one open string required');
+    expect(() => new Tuning(description)).toThrow(expectedError);
+  });
+  
+  test.each([
+    ['CE',     ['C', 'E']],
+    ['C# F',   ['C#', 'F']],
+    ['D4F#4',  ['D4', 'F#4']],
+    ['D#4 G4', ['D#4', 'G4']],
+    ['E G#4',  ['E', 'G#4']],
+    ['FA4',    ['F', 'A4']],
+    ['Bb2',    ['A#2']],
+  ])('openStrings', (description, expectedOpenStrings) => {
+    expect(new Tuning(description).openStrings).toEqual(expectedOpenStrings.map(n => new Note(n)));
+  });
+  
+  test.each([
+    ['CE',     'Tuning("C E")'],
+    ['C# F',   'Tuning("C# F")'],
+    ['D4F#4',  'Tuning("D4 F#4")'],
+    ['D#4 G4', 'Tuning("D#4 G4")'],
+    ['E G#4',  'Tuning("E G#4")'],
+    ['FA4',    'Tuning("F A4")'],
+    ['Bb2',    'Tuning("A#2")'],
+  ])('toString', (description, expectedString) => {
+    expect(new Tuning(description).toString()).toBe(expectedString);
+  });
+  
+  test.each([
+    '?', 'O', 'A-B'
+  ])('invalid', description => {
+    expect(() => new Tuning(description)).toThrow(SyntaxError);
+  });
+  
+  test.each([
+    {
+      description: 'C4',
+      chordName: 'C',
+      fretCount: 0,
+      fretboard: [['C4']]
+    },
+    {
+      description: 'A1 D2',
+      chordName: 'F#6',
+      fretCount: 9,
+      fretboard: [
+        [null, 'A#1', null, null, 'C#2', null, 'D#2', null, null,  'F#2'],
+        [null, 'D#2', null, null, 'F#2', null, null,  null, 'A#2', null ],
+      ]
+    },
+    {
+      description: 'G C E A',
+      chordName: 'Cm7',
+      fretCount: 6,
+      fretboard: [
+        ['G',  null, null, 'A#', null, 'C',  null],
+        ['C',  null, null, 'D#', null, null, null],
+        [null, null, null, 'G',  null, null, 'A#'],
+        [null, 'A#', null, 'C',  null, null, 'D#'],
+      ]
+    },
+    {
+      description: 'D#3',
+      chordName: 'Edim',
+      fretCount: 13,
+      fretboard: [
+        [null, 'E3', null, null, 'G3', null, null, 'A#3', null, null, null, null, null, 'E4'],
+      ]
+    },
+  ])('getFretboard', ({ description, chordName, fretCount, fretboard }) => {
+    const tuning = new Tuning(description);
+    const chord = new Chord(chordName);
+    const expectedFretboard = fretboard.map(s => s.map(n => n !== null ? new Note(n) : null));
+    const actualFretboard = tuning.getFretboard(chord, fretCount);
+    expect(actualFretboard).toEqual(expectedFretboard);
+  });
+  
+  test.each([
+    ['C',  'C'],
+    ['C4', 'C'],
+  ])('getGroup: undefined group', (root, note) => {
+    const group = getGroup(new Note(root), new Note(note));
+    expect(group).toBeUndefined();
+  });
+  
+  test.each([
+    ['C4',  'C4', 'C#4'],
+    ['E0',  'E8', 'Eb9'],
+    ['C4',  'A5', 'B5'],
+    ['A4',  'C5', 'B4'],
+    ['G#3', 'G4', 'G#3'],
+  ])('getGroup: same group', (root, note1, note2) => {
+    const rootNote = new Note(root);
+    const group1 = getGroup(rootNote, new Note(note1));
+    const group2 = getGroup(rootNote, new Note(note2));
+    expect(group1).toBeDefined();
+    expect(group1).toEqual(group2);
+  });
+  
+  test.each([
+    ['C4',  'C4', 'C5'],
+    ['E0',  'E9', 'Eb9'],
+    ['C4',  'A3', 'B5'],
+    ['A4',  'C5', 'B5'],
+    ['G#3', 'G3', 'G#3'],
+  ])('getGroup: different groups', (root, note1, note2) => {
+    const rootNote = new Note(root);
+    const group1 = getGroup(rootNote, new Note(note1));
+    const group2 = getGroup(rootNote, new Note(note2));
+    expect(group1).toBeDefined();
+    expect(group2).toBeDefined();
+    expect(group1).not.toEqual(group2);
+  });
+
+});
