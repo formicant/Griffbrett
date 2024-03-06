ce77c2a1b66cb881e296e4fbebcfca8880aa0e8a
formicant <formicant@gmail.com>
2024-03-06 13:13:46 +0700

    Small ui refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: {
-    main: "./src/main.ts",
+    main: "./src/index.ts",
   },
   output: {
     path: path.resolve(__dirname, './public'),
