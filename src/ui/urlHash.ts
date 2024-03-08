99597ec4ed9813003953e6ad3890ee9e44965497
formicant <formicant@gmail.com>
2024-03-09 00:37:49 +0700

    Add instrument to url hash

--- src/ui/urlHash.ts
+++ src/ui/urlHash.ts
@@ -1,8 +1,6 @@
 import { Model, defaultModel } from './model';
 import { removeTypography } from './typography';
 
-const formatVersion = 0;
-
 /**
  * Encodes the model into the page's URL hash part.
  * This allows the user to share links to specific tunings and chords.
@@ -10,7 +8,7 @@ const formatVersion = 0;
 export function setUrlHash(model: Model) {
   const tuningDescription = removeTypography(model.tuningDescription).trim().replace(/\s+/g, '-');
   const chordName = removeTypography(model.chordName).trim();
-  const hash = `${formatVersion}|${tuningDescription}|${model.fretCount}|${chordName}`;
+  const hash = `${model.instrument}|${tuningDescription}|${model.fretCount}|${chordName}`;
   window.location.hash = hash;
 }
 
@@ -23,12 +21,11 @@ export function getUrlHash(): Model {
   try {
     const text = decodeURI(window.location.hash.replace(/#/, ''));
     const parts = text.split('|');
-    if (parseInt(parts[0]) === formatVersion) {
-      const tuningDescription = parts[1].replace(/-/g, ' ');
-      const fretCount = parseInt(parts[2]);
-      const chordName = parts[3];
-      return { instrument: '', tuningDescription, fretCount, chordName };
-    }
+    const instrument = parts[0];
+    const tuningDescription = parts[1].replace(/-/g, ' ');
+    const fretCount = parseInt(parts[2]);
+    const chordName = parts[3];
+    return { instrument, tuningDescription, fretCount, chordName };
   } catch { }
   return defaultModel;
 }
